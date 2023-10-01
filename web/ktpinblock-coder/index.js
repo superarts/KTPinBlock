d24b1fa3d38914521feb95600d24e2fa9c14be3b
Leo Liu <leoliu8@deloitte.com>
2023-09-30 20:42:22 -0400

    #2: updated web encoding sample

--- web/ktpinblock-coder/index.js
+++ web/ktpinblock-coder/index.js
@@ -14,17 +14,28 @@ const {
 } = require("data-crypto");
 */
 
-console.log('start')
-
 const pan = "1111222233334444";
 const pin = "1234";
 
+console.log("PAN: " + pan)
+console.log("PIN: " + pin)
+
+console.log('encoding...')
+
 // console.log(JSON.stringify(kotlin, null, 4))
 // console.log(Object.keys(kotlin))
-console.log(kotlin.org.superarts.ktpinblock.PinBlock)
+const ktpinblock = new kotlin.org.superarts.ktpinblock.PinBlock
+const format = kotlin.org.superarts.ktpinblock.format.PinBlockFormat
+//console.log("PinBlock: %o", ktpinblock)
+//console.log("PinBlock: %o", kotlin.org.superarts.ktpinblock.PinBlock.prototype)
 //console.log(Object.getOwnPropertyNames(kotlin.org.superarts.ktpinblock.PinBlock))
-//console.log(kotlin.org.superarts.ktpinblock.format.PinBlockFormat)
-//const block = kotlin.org.superarts.ktpinblock.PinBlock().encode(pan, pin, format)
-//console.log(block)
-
-console.log('done')
+//console.log("PinBlockFormat: %o", kotlin.org.superarts.ktpinblock.format.PinBlockFormat.ISO3)
+//const block = kotlin.org.superarts.ktpinblock.PinBlock.prototype.encode(pan, pin, kotlin.org.superarts.ktpinblock.format.PinBlockFormat.ISO3)
+const block0 = ktpinblock.encode(pan, pin, format.ISO0)
+const block1 = ktpinblock.encode(null, pin, format.ISO1)
+const block2 = ktpinblock.encode(null, pin, format.ISO2)
+const block3 = ktpinblock.encode(pan, pin, format.ISO3)
+console.log("iso0: " + block0)
+console.log("iso1: " + block1)
+console.log("iso2: " + block2)
+console.log("iso3: " + block3)
